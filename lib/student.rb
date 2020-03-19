7485de71b5c65e68f2a803571828ea91d995e8d0
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 06:28:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,16 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  @@all =[]
+  
+  
+  def initialize(name, grade, id= nil)
+    @name = name 
+    @grade = grade 
+    @id = id
+  end
+  
 end
